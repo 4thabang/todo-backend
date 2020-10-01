737fc959681930dc6e736c7f91145f6002f2eba9
4thabang <thabang@fordabl.com>
2020-10-01 10:45:33 +0100

    commit

--- cmd/models/user.go
+++ cmd/models/user.go
@@ -1,10 +1,13 @@
 package models
 
+import "time"
+
 // User Model
 type User struct {
 	ID             uint `gorm:"primaryKey"`
 	Name           string
 	Username       string
+	CreatedAt      time.Time
 	CompletedTodos int
 	ActiveTodos    int
 }
