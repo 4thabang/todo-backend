8c383bcd44c97b9334022c62edaf5e7beb591e34
4thabang <thabang@fordabl.com>
2020-10-02 20:53:26 +0100

    commit

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+	"github.com/gofiber/fiber/v2"
 )
 
 type todos struct {
@@ -15,36 +11,18 @@ type todos struct {
 	Completed bool   `json:"completed"`
 }
 
+func fiberGet() {
+}
+
 func main() {
-	http.HandleFunc("/todos", TodoServer)
-	http.ListenAndServe(":8080", nil)
+	app := fiber.New()
+
+	app.Get("/", TodoServer)
+
+	app.Listen(":8080")
 }
 
 // TodoServer Req/Res
-func TodoServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	get()
-}
-
-func get() {
-	fmt.Println("Get Request")
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos")
-	if err != nil {
-		log.Fatalln(err)
-		return
-	}
-
-	defer resp.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-		return
-	}
-
-	bodyString := string(bodyBytes)
-	fmt.Println("API Res:\n" + bodyString)
-
-	var todoStruct todos
-	json.Unmarshal(bodyBytes, &todoStruct)
-	fmt.Printf("API Res Struct %+v\n", todoStruct)
+func TodoServer(c *fiber.Ctx) error {
+	return c.SendString("Hello, world!")
 }
