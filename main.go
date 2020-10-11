88b13dcb477d3f84360d32e2748d978b108764c1
4thabang <thabang@fordabl.com>
2020-10-11 21:36:11 +0100

    feature(delete): allow anyone to delete an entry

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -38,16 +39,23 @@ var (
 func main() {
 	router.Use(middleware.Logger)
 
+	router.Get("/", home)
 	router.Post("/create", createEvent)
 	router.Get("/events", getEvents)
 	router.Get("/events/{id}", getOneEvent)
+	router.Delete("/events/{id}", deleteEvent)
 
-	fmt.Println("Listening on port :8080")
+	fmt.Println("\nListening on port :8080")
 
 	http.ListenAndServe(":8080", router)
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello world. Welcome home")
+}
+
 func createEvent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	// Gets 'event' and uses its type structure
 	var newEvent event
 	// Reads the body entry
@@ -55,15 +63,28 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, "kindly enter data with the event title and desc")
 	}
-
 	// Takes body entry value and passes it into `newEvent` in it's set structure
 	json.Unmarshal(reqBody, &newEvent)
 	// We simply append the 'newEvent' entry into the 'events' variable (database)
 	events = append(events, newEvent)
 	// HTTP status code in header
 	w.WriteHeader(http.StatusCreated)
+	// Writes to the body in JSON format
+	err = json.NewEncoder(w).Encode(newEvent)
+	if err != nil {
+		log.Fatalf("Error: %v\n", err)
+	}
+}
 
-	json.NewEncoder(w).Encode(newEvent)
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := chi.URLParam(r, "id")
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			events = append(events[:i], events[i+1:]...)
+			fmt.Fprintf(w, "The event with ID: %v.\nHas been deleted successfully", eventID)
+		}
+	}
 }
 
 func getOneEvent(w http.ResponseWriter, r *http.Request) {
@@ -72,7 +93,10 @@ func getOneEvent(w http.ResponseWriter, r *http.Request) {
 
 	for _, singleEvent := range events {
 		if singleEvent.ID == eventID {
-			json.NewEncoder(w).Encode(singleEvent)
+			err := json.NewEncoder(w).Encode(singleEvent)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 }
