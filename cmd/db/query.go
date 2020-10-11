8eefbe3f2b64420a939b9b999e7d63540b7ee81b
4thabang <thabang@fordabl.com>
2020-10-11 15:51:10 +0100

    feature(json): play with json unmarshal

--- cmd/db/query.go
+++ cmd/db/query.go
@@ -1,8 +1,35 @@
 package db
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	GoBrr bool   `json:"brr"`
+}
+
+var persons = []byte(`[
+	{
+		Name:  "Thabang",
+		Age:   20,
+		GoBrr: true,
+	},
+	{
+		Name:  "Divine",
+		Age:   2,
+		GoBrr: true,
+	},
+]`)
 
 // QueryDB is where we read from our DB
 func QueryDB() {
-	fmt.Println("Query a DB")
+	var people []person
+	err := json.Unmarshal(persons, &people)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(people)
 }
