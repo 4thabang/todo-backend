ac435821251b674e006ad8cef7267d77dcff0753
4thabang <thabang@fordabl.com>
2020-10-13 23:05:14 +0100

    commit

--- cmd/handler/handler.go
+++ cmd/handler/handler.go
@@ -3,9 +3,10 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 // Handler allows us to play with our api
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World!")
+	fmt.Fprint(w, time.Second*10)
 }
