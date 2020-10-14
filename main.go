7aa3aff33ae8f7c256a2bf436657793c2c833757
4thabang <thabang@fordabl.com>
2020-10-14 10:37:31 +0100

    refactor(main): refactor the code

--- main.go
+++ main.go
@@ -1,139 +1,27 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
+
 	"net/http"
 
-	"github.com/4thabang/todo-backend/cmd/handler"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
-type event struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-}
-
-type allEvents []event
-
-var events = allEvents{
-	{
-		ID:          "1",
-		Title:       "Introducing golang",
-		Description: "Come join us for a chance to learn how go",
-	},
-	{
-		ID:          "2",
-		Title:       "This is a rest api",
-		Description: "Rest apis are cool and should be used often",
-	},
-}
-
 func main() {
-	router := chi.NewRouter()
+	r := chi.NewRouter()
 
-	router.Use(middleware.Logger)
+	r.Use(middleware.Logger)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
 
-	router.Get("/", handler.Handler)
-	router.Get("/api", fetchAPI)
-	router.Post("/create", createEvent)
-	router.Get("/events", getEvents)
-	router.Get("/events/{id}", getOneEvent)
-	router.Delete("/events/{id}", deleteEvent)
-	router.Get("/ping", ping)
+	r.Get("/", httpServe)
 
-	fmt.Println("\nListening on port :8080")
-
-	err := http.ListenAndServe(":8080", router)
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.ListenAndServe(":8080", r)
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello world. Welcome home")
-}
-
-func ping(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Pong")
-}
-
-type todos struct {
-	UserID     int    `json:"userId"`
-	ID         int    `json:"id"`
-	Title      string `json:"title"`
-	Complteted bool   `json:"completed"`
-}
-
-func fetchAPI(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Type", "application/json")
-
-	res, err := http.Get("https://jsonplaceholder.typicode.com/todos")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var todo todos
-	json.Unmarshal(data, &todo)
-	fmt.Fprint(w, string(data))
-}
-
-func createEvent(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var newEvent event
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "kindly enter data with the event title and desc")
-	}
-
-	json.Unmarshal(reqBody, &newEvent)
-	events = append(events, newEvent)
-
-	w.WriteHeader(http.StatusCreated)
-
-	if http.StatusOK == 200 {
-		fmt.Fprintln(w, "Post has been added!")
-	}
-}
-
-func deleteEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := chi.URLParam(r, "id")
-
-	for i, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			events = append(events[:i], events[i+1:]...)
-			fmt.Fprintf(w, "The event with ID: %v.\nHas been deleted successfully", eventID)
-		}
-	}
-}
-
-func getOneEvent(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	eventID := chi.URLParam(r, "id")
-
-	for _, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			err := json.NewEncoder(w).Encode(singleEvent)
-			if err != nil {
-				panic(err)
-			}
-		}
-	}
-}
-
-func getEvents(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	json.NewEncoder(w).Encode(events)
+func httpServe(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
 }
