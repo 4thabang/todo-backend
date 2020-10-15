5727dc2c50c830d2a6a2b66b45478e4632b9d663
4thabang <thabang@fordabl.com>
2020-10-15 13:40:12 +0100

    commit

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	"net/http"
 
@@ -23,6 +26,27 @@ func main() {
 	http.ListenAndServe(":8080", r)
 }
 
+type todoStr struct {
+	UserID int    `json:"userId"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
 func httpServe(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+	w.Header().Set("Content-Type", "application/json")
+	res, err := http.Get("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(data, &todoStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprint(w, string(data))
 }
